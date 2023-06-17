191f7640e373a810018bc968480a852ca8571b83
julian <jnsoretniuq1998@gmail.com>
2023-06-17 12:56:20 -0300

    termino

--- Empleado.cs
+++ Empleado.cs
@@ -1,43 +1,111 @@
 namespace EmpleadoTp
 {
-        enum cargo   //constantes en enum
+    public enum cargoEmpleado   //constantes en enum
+    {
+        Auxiliar,
+        Administrativo,
+        Ingeniero,
+        Especialista,
+        Investigador,
+    }
+
+    public class Empleado
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime fechNac;
+        private char genero;
+        private DateTime fechIngreso;
+        private double sueldoBasico;
+        private cargoEmpleado cargo;
+
+        public string Nombre
+        { get => nombre; set => nombre = value; }
+        public string Apellido
+        { get => apellido; set => apellido = value; }
+        public DateTime FechNac
+        { get => fechNac; set => fechNac = value; }
+        public char Genero
+        { get => genero; set => genero = value; }
+        public DateTime FechIngreso
+        { get => fechIngreso; set => fechIngreso = value; }
+        public double SueldoBasico
+        { get => sueldoBasico; set => sueldoBasico = value; }
+        public cargoEmpleado Cargo { get => cargo; set => cargo = value; }
+
+        public int Antiguaedad()
         {
-            Auxiliar = 0,
-            Administrativo = 1,
-            Ingeniero = 2,
-            Especialista = 3,
-            Investigador = 4,
+            //int antiguedad;
+            var fechaActual = DateTime.Now;
+            TimeSpan diferencia = fechaActual.Subtract(FechIngreso);
+            int antiguedadAnios = diferencia.Days / 365;
+            //antiguedad = "Antigüedad del empleado: " + antiguedadAnios + " años, " + antiguedadMeses + " meses y " + antiguedadDias + " días";
+            return (antiguedadAnios);
         }
-        public class Empleado
+
+        public int edadEmpleado()
         {
-            private string nombre;
-            private string apellido;
-            private DateTime fechNac;
-            private char genero;
-            private DateTime fechIngreso;
-            private double sueldoBasico;
-            private cargo cargo;
+            var anioActual = DateTime.Now;
+            int retorno;
+            int edad = anioActual.Year - FechNac.Year;
+            retorno = edad;
+            return retorno;
+        }
 
-            public string Nombre
-            { get => nombre; set => nombre = value; }
-            public string Apellido
-            { get => apellido; set => apellido = value; }
-            public DateTime FechNac
-            { get => fechNac; set => fechNac = value; }
-            public char Genero
-            { get => genero; set => genero = value; }
-            public DateTime FechIngreso
-            { get => fechIngreso; set => fechIngreso = value; }
-            public double SueldoBasico
-            { get => sueldoBasico; set => sueldoBasico = value; }
-            internal cargo Cargo
-            { get => cargo; set => cargo = value; }
-
-            public TimeSpan Antiguaedad(DateTime fechaIngreso)
+        public int jubilo(int edad)
+        {
+            int faltan;
+            if (Genero == 'm' || Genero == 'M')
             {
-                DateTime actual = DateTime.Today;
-                TimeSpan antiguedad = actual - fechaIngreso;
-                return (antiguedad);
+                if (edad < 65)
+                {
+                    faltan = 65 - edad;
+                    return faltan;
+                }
+
             }
+            else
+            {
+                if (edad < 60)
+                {
+                    faltan = 60 - edad;
+                    return faltan;
+                }
+            }
+
+            return 0;
+
+
         }
+        public double Salario(int antiguedad, int estadoCivil)
+        {
+            double SalarioNeto;
+            double Adicional;
+
+            if (antiguedad <= 20)
+            {
+                Adicional = (SueldoBasico / 100) * antiguedad;
+
+            }
+            else
+            {
+                Adicional = (SueldoBasico * 25 / 100) * antiguedad;
+            }
+            if (Cargo == cargoEmpleado.Ingeniero || Cargo == cargoEmpleado.Especialista)
+            {
+                Adicional += (Adicional * 50) / 100;
+            }
+
+            if (estadoCivil == 1)
+            {
+                Adicional += 15000;
+            }
+
+            SalarioNeto = Adicional + SueldoBasico;
+
+            return SalarioNeto;
+
+        }
+
+    }
 }
